00ea70ababde0fe0f495e5d7468b923a3f208491
Lukasz Zatyka <lukasz.zatyka@gmail.com>
2022-10-22 18:47:06 +0200

    Implemented datoCMS blog posts

--- lib/constans.ts
+++
@@ -1,2 +0,0 @@
-export const API_URL = 'https://graphql.datocms.com';
-export const API_TOKEN = process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN;
--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-// require('dotenv').config();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   images: {
     domains: ['www.datocms-assets.com'],
   },
 };
-
-module.exports = {
-  env: {
-    NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN: process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
-  },
-};
-
-module.exports = nextConfig;
